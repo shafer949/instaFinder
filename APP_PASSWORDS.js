23f0324910a91bcbb6963b95e4a8fd3d6b27d77f
Michael Liendo <model3volution@gmail.com>
2016-10-24 14:53:32 -0500

    working method of getting data

--- APP_PASSWORDS.js
+++ APP_PASSWORDS.js
@@ -1,7 +1,9 @@
 var APP_PASSWORDS = {
   clientID: '82d957a8aace4d6ea06105d11838a79a',
   clientSecret: '8bcc14fe04a846dfba63e0be8ea62732',
-  redirectUrl: 'http://qctimes.com/entertainment'
+  redirectUrl: 'http://qctimes.com/entertainment/',
+  code: 'f4630e5e48fe454dac730aaf131476fd',
+  accessToken: '212658747.82d957a.0e3c1ccc961c40db94288b08f27115aa'
 };
 
-module.exports.passwords = APP_PASSWORDS;
+module.exports.APP_PASSWORDS = APP_PASSWORDS;
