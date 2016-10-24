c626d8f51031dba68dade503220966998cb0037b
Michael Liendo <model3volution@gmail.com>
2016-10-24 12:27:21 -0500

    updated to follow namespace convention

---
+++ APP_PASSWORDS.js
@@ -0,0 +1,7 @@
+var APP_PASSWORDS = {
+  clientID: '82d957a8aace4d6ea06105d11838a79a',
+  clientSecret: '8bcc14fe04a846dfba63e0be8ea62732',
+  redirectUrl: 'http://qctimes.com/entertainment'
+};
+
+module.exports.passwords = APP_PASSWORDS;
