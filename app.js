da73375e35fb30d04e38869f1d75b56a527d8252
Michael Liendo <model3volution@gmail.com>
2016-12-16 09:54:40 -0600

    recruiter ready revisions

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const {eachOfSeries} = require('async');
 
-const {APP_PASSWORDS} = require('./APP_PASSWORDS');
-const requestData = require('./requestData');
-const saveImages = require('./saveImages');
-const photoMetaArr  = require('./instaConfig');
+const {APP_PASSWORDS} = require('./APP_PASSWORDS'); //remove from .gitignore for interview process
+const requestData = require('./utils/requestData');
+const saveImages = require('./utils/saveImages');
+const photoMetaArr  = require('./utils/instaConfig');
 
 const baseUrl = `https://api.instagram.com/v1/media/search?access_token=`;
 
+/*
+Foreach location I specified in instaConfig.js, give me all of the photos in that location
+(Instagram api only allows a location to give the last 20 photos), then go through each photo
+and save it to the fs with relevant data in a .txt file (in downloads folder), once that location is
+complete, move on to the next one.
+*/
 eachOfSeries(photoMetaArr, (value, key, outerCallback) => {
   console.log('--------fired:', key);
   const {lat, lng, distance} = value;
   console.log(photoMetaArr[key].lat);
   let dataURL = `${baseUrl}${APP_PASSWORDS.accessToken}&lat=${lat}&lng=${lng}&distance=${distance}`;
-   requestData(dataURL, true).then((instaData) => saveImages(instaData, key, outerCallback),(err) => {
+   requestData(dataURL, true).then((instaData) => {
+     var indivLocPhotos = saveImages(instaData, key, outerCallback);
+   },(err) => {
      console.log(err);
    });
+}, () => {
+  return
 });
--- utils/inPast.js
+++ utils/inPast.js
@@ -1,7 +1,9 @@
 const moment = require('moment');
 /*
 supports:
-
+if a TIMEFRAME env var is specified, moment will use that,
+useful in cases where the user wants to get more photos in the past
+(keep in mind that Instagram only allows for the last 20 photos to be retrieved--no pagination)
 */
 const timeFrame = process.env.TIMEFRAME || "days";
 let isWithinPast = (pastAmt,timeItemCreated) => {
