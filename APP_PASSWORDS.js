a9fe809f798bcdc23dd1eb6ddf4130a6dd3b81e0
Michael Liendo <model3volution@gmail.com>
2016-12-16 10:02:32 -0600

    dev passwords for testing

---
+++ APP_PASSWORDS.js
@@ -0,0 +1,9 @@
+var APP_PASSWORDS = {
+  clientID: '82d957a8aace4d6ea06105d11838a79a',
+  clientSecret: '8bcc14fe04a846dfba63e0be8ea62732',
+  redirectUrl: 'http://qctimes.com/entertainment/',
+  code: 'f4630e5e48fe454dac730aaf131476fd',
+  accessToken: '212658747.82d957a.0e3c1ccc961c40db94288b08f27115aa'
+};
+
+module.exports.APP_PASSWORDS = APP_PASSWORDS;
